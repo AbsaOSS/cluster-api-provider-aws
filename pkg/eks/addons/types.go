785629d2f63e3c44675c72ee45d1c585f45faf93
Michel Loiseleur <michel.loiseleur@traefik.io>
2025-04-07 12:03:25 +0200

    fix(addons): avoid infinite reconciliation loop

--- pkg/eks/addons/types.go
+++ pkg/eks/addons/types.go
@@ -36,7 +36,7 @@ type EKSAddon struct {
 
 // IsEqual determines if 2 EKSAddon are equal.
 func (e *EKSAddon) IsEqual(other *EKSAddon, includeTags bool) bool {
-	//NOTE: we do not compare the ARN as that is only for existing addons
+	// NOTE: we do not compare the ARN as that is only for existing addons
 	if e == other {
 		return true
 	}
@@ -47,9 +47,6 @@ func (e *EKSAddon) IsEqual(other *EKSAddon, includeTags bool) bool {
 		return false
 	}
 	if !cmp.Equal(e.Configuration, other.Configuration) && other.Configuration != nil {
-		return false
-	}
-	if !cmp.Equal(e.ResolveConflict, other.ResolveConflict) && other.ResolveConflict != nil {
 		return false
 	}
 
